257e26a17cf9153fd06c1cb150506b6f515c6935
josh <joshua.boufini@gmail.com>
2018-04-25 18:49:30 +0100

    comit

--- site/Home.php
+++ site/Home.php
@@ -5,6 +5,7 @@
             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
         </ol>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <div class="carousel-inner" role="listbox">
             
             <div class="item active slide1" style="background-image:url(build/img/slider1.jpg)">
